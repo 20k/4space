038800ddf2b5c74b618d9dcd7f5f280ca81b962d
James <Icedshot@gmail.com>
2017-04-06 06:43:18 +0100

    notes on event system

--- event_system.hpp
+++ event_system.hpp
@@ -4,11 +4,19 @@
 namespace game_event_info
 {
     ///star trek
+    ///basic mechanics: Scanning, salvage, combat, research, player -> lore interaction, resources, diplomacy
+    ///Implement these first
+    ///possible consequences: New tech, new ship, more resources, hostile ship spawning, hostile crew boarding, ship damaged
+    ///new planet, new asteroids (resource), new solar system, ship destroyed, crew destroyed, lose tech, lose resources,
+    ///empire being born, planet colonised, planet taken over by hostile species, robot uprising (new empire?), robot augmentation,
+    ///ship augmentation, solar system destroyed, planet wiped, diplomacy
+    ///region of space mapped, region of space unmapped, hidden secrets uncovered (dark planet/belt/stars)
     enum types
     {
         LOCAL_UPRISING, ///revolts on planet. Needs space -> planet interactiont to work
         DARK_PLANET, ///missing planet. Needs scanners to work fully, basic exploration will be a stopgap
         DARK_ASTEROID_BELT, ///missing belt, see above
+        DARK_STAR, ///missing star, see above
         LOCAL_NATIVES, ///natives on planet doing something. Needs space -> planet, very little else (this is more lore)
         ANCIENT_PRECURSOR, ///found something. Includes alien tech, traces of civilisation.
         LONE_DERELICT, ///individual derelict, needs salvage mechanics and combat to work, research would help. Could be ancient
@@ -17,7 +25,7 @@ namespace game_event_info
         ///Ringworld
         ///AI being born
         ///AI revolt (tie into above?)
-        ///
+        ///something wrong with the star
     };
 }
 
