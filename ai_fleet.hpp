ae78ea8aa2a0de303c4227193c40e4a370fc0200
James <Icedshot@gmail.com>
2017-04-09 19:45:12 +0100

    beginnings of fleet ai

---
+++ ai_fleet.hpp
@@ -0,0 +1,20 @@
+#ifndef AI_FLEET_HPP_INCLUDED
+#define AI_FLEET_HPP_INCLUDED
+
+struct ship_manager;
+
+///fleet manager owns the ai_fleet?
+///would simplify dependency management as it all already works fine
+///but then... difficult to centralise
+///I suppose fleets are the only thing that needs to be centralised
+///and we're talking about fleet ai which is super low level, like engage enemies
+///but then it needs high level control and strategy, like take fleet between systems
+///higher level ai ie embedded in empire will have access to fleets, and i guess their fleet ai
+///so can give direction to ai fleets in there
+///makes sense, lets roll
+struct ai_fleet
+{
+    void tick_fleet(ship_manager* ship_manage);
+};
+
+#endif // AI_FLEET_HPP_INCLUDED
