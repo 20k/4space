d3e73635556b80e976d6695bcc4c0d40d57a14ee
James <Icedshot@gmail.com>
2017-04-17 13:07:59 +0100

    minor adjustments pre new generation

--- procedural_text_generator.hpp
+++ procedural_text_generator.hpp
@@ -12,19 +12,18 @@ namespace procedural_text
     std::vector<std::string> parse_star_names();
 
     static std::vector<std::string> planet_names = parse_planet_names();
-
     static std::vector<std::string> star_names = parse_star_names();
 }
 
 struct procedural_text_generator
 {
     std::string generate_planetary_name();
-
     std::string generate_planetary_text(orbital* o);
 
     std::string generate_star_name();
-
     std::string generate_star_text(orbital* o);
+
+    ///ship names, and empire names
 };
 
 #endif // PROCEDURAL_TEXT_GENERATOR_HPP_INCLUDED
