715be60f40191e5648c1f772b3c7b1fec32069c1
James <Icedshot@gmail.com>
2017-07-29 05:53:23 +0100

    delete old code

--- popup.hpp
+++ popup.hpp
@@ -22,38 +22,12 @@ namespace popup_element_type
     };
 }
 
-struct button_element
-{
-    std::string name;
-    bool pressed = false;
-};
-
 struct popup_element
 {
     static int gid;
     int id = gid++;
 
-    //std::string header;
-    //std::vector<std::string> data;
-    std::deque<bool> checked;
-    //bool mergeable = false;
-
-    std::map<popup_element_type::types, button_element> buttons_map;
-
     void* element = nullptr;
-
-    void try_set_button_map(bool should_set, popup_element_type::types type, const std::string& name)
-    {
-        if(!should_set)
-        {
-            buttons_map.erase(type);
-            return;
-        }
-        else
-        {
-            buttons_map[type].name = name;
-        }
-    }
 };
 
 struct popup_info
@@ -86,31 +60,6 @@ struct popup_info
     bool going = false;
 
     bool declaring_war = false;
-
-    /*bool started = false;
-
-    void imgui_begin()
-    {
-        if(elements.size() == 0)
-            going = false;
-
-        if(!going)
-            return;
-
-        started = true;
-
-        ImGui::Begin(("Selected###INFO_PANEL"), nullptr, ImVec2(0,0), -1.f, ImGuiWindowFlags_AlwaysAutoResize | IMGUI_WINDOW_FLAGS);
-    }
-
-    void imgui_end()
-    {
-        if(!started)
-            return;
-
-        ImGui::End();
-
-        started = false;
-    }*/
 };
 
 
