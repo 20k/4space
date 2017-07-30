3cee36d0c088f363b3aeb6aa8ebce66feb639d16
James <Icedshot@gmail.com>
2017-07-30 07:07:15 +0100

    fun with dragging

--- drag_and_drop.cpp
+++ drag_and_drop.cpp
@@ -1,1 +1,32 @@
 #include "drag_and_drop.hpp"
+
+void drag_and_drop::begin_drag_section(const std::string& tag)
+{
+    current_tag = tag;
+}
+
+void drag_and_drop::tick_locking_window()
+{
+    window_info& inf = window_map[current_tag];
+
+    if(inf.locked)
+    {
+        ImGui::SetWindowPos({inf.pos.x(), inf.pos.y()});
+    }
+}
+
+void drag_and_drop::begin_dragging(void* data, drag_and_drop_info::type type)
+{
+    window_info& inf = window_map[current_tag];
+
+    inf.locked = true;
+    inf.pos = {ImGui::GetWindowPos().x, ImGui::GetWindowPos().y};
+}
+
+void drag_and_drop::finish_dragging()
+{
+    for(auto& i : window_map)
+    {
+        i.second.locked = false;
+    }
+}
--- drag_and_drop.hpp
+++ drag_and_drop.hpp
@@ -1,6 +1,46 @@
 #ifndef DRAG_AND_DROP_HPP_INCLUDED
 #define DRAG_AND_DROP_HPP_INCLUDED
 
+#include <imgui/imgui.h>
+#include <string>
+#include <map>
+#include <vec/vec.hpp>
 
+namespace drag_and_drop_info
+{
+    enum type
+    {
+        NONE = 0,
+        ORBITAL,
+    };
+}
+
+struct window_info
+{
+    vec2f pos;
+    bool locked = false;
+};
+
+struct drag_and_drop
+{
+    bool dragging = false;
+    void* data = nullptr;
+
+    std::map<std::string, window_info> window_map;
+
+    drag_and_drop_info::type currently_dragging = drag_and_drop_info::NONE;
+
+    vec2f locked_window_pos = {0,0};
+
+    std::string current_tag;
+
+    void begin_drag_section(const std::string& tag);
+    void end_drag_section(const std::string& tag);
+
+    void tick_locking_window();
+
+    void begin_dragging(void* data, drag_and_drop_info::type type);
+    void finish_dragging();
+};
 
 #endif // DRAG_AND_DROP_HPP_INCLUDED
