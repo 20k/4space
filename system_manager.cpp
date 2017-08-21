0b0834b96cb55bc8d223aab7a9f7c53e31f5f56c
James <Icedshot@gmail.com>
2017-08-21 11:29:43 +0100

    Thanks -wmisleading-indentation!

--- system_manager.cpp
+++ system_manager.cpp
@@ -58,7 +58,7 @@ bool operator<(const empire_popup& e1, const empire_popup& e2)
         return e1.e->team_id < e2.e->team_id;
     }
 
-    if(e1.type != e2.type);
+    if(e1.type != e2.type)
     {
         return e1.type < e2.type;
     }
