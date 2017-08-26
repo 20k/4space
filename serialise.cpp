d2c093dd452350dc441e4e90a3e18f2020963fb4
James <Icedshot@gmail.com>
2017-08-26 16:21:14 +0100

    beginnings of serialisation

---
+++ serialise.cpp
@@ -0,0 +1,3 @@
+#include "serialise.hpp"
+
+uint64_t serialisable::gserialise_id;
---
+++ serialise.hpp
@@ -0,0 +1,170 @@
+#ifndef SERIALISE_HPP_INCLUDED
+#define SERIALISE_HPP_INCLUDED
+
+#include <new>
+#include <vector>
+#include <stdint.h>
+#include <iostream>
+#include <map>
+
+struct serialisable
+{
+    static uint64_t gserialise_id;
+    uint64_t serialise_id = gserialise_id++;
+    //uint64_t network_id = serialise_id;
+
+    bool owned = true;
+    int32_t owner_id = 0;
+
+    bool handled_by_client = true;
+
+    //static std::map<void*, uint64_t> pointer_to_id;
+
+    ///recieving
+    //static std::map<uint64_t, void*> owned_id_to_pointer;
+    static std::map<int32_t, std::map<uint64_t, void*>> owner_to_id_to_pointer;
+};
+
+template<typename T>
+struct serialise_helper
+{
+    void add(T v, std::vector<char>& data)
+    {
+        char* pv = std::launder((char*)&v);
+
+        for(uint32_t i=0; i<sizeof(T); i++)
+        {
+            data.push_back(pv[i]);
+        }
+    }
+
+    T get(int& internal_counter, std::vector<char>& data)
+    {
+        int prev = internal_counter;
+
+        internal_counter += sizeof(T);
+
+        if(internal_counter > (int)data.size())
+        {
+            std::cout << "Error, invalid bytefetch" << std::endl;
+
+            return T();
+        }
+
+        return *std::launder((T*)&data[prev]);
+    }
+};
+
+template<typename T>
+struct serialise_helper<T*>
+{
+    void add(T* v, std::vector<char>& data)
+    {
+        //serialisable::pointer_to_id[v] = v->serialise_id;
+
+        /*char* pv = std::launder((char*)v);
+
+        for(uint32_t i=0; i<sizeof(T); i++)
+        {
+            data.push_back(pv[i]);
+        }*/
+
+        serialise_helper<decltype(serialisable::owner_id)> helper_owner_id;
+        serialise_helper<decltype(serialisable::serialise_id)> helper1;
+
+        helper_owner_id.add(v->owner_id, data);
+        helper1.add(v->serialise_id, data);
+
+        /*if(v->owned)
+        {
+        }
+        else
+        {
+
+            helper1.add(serialisable::owner_to_id_to_pointer[v->owner][v], data);
+        }*/
+
+        serialise_helper<T> helper2;
+        helper2.add(*v, data);
+    }
+
+    ///ok. So. we're specialsied on a pointer which means we're requesting a pointer
+    ///but in reality when we serialise, we're going to get our 64bit id followed by the data of the pointer
+
+    T* get(int& internal_counter, std::vector<char>& data)
+    {
+        serialise_helper<decltype(serialisable::owner_id)> helper_owner_id;
+        int32_t owner_id = helper_owner_id.get(internal_counter, data);
+
+        serialise_helper<decltype(serialisable::serialise_id)> helper1;
+        uint64_t id = helper1.get(internal_counter, data);
+
+        //T* ptr = (T*)serialisable::id_to_pointer[id];
+
+        T* ptr = (T*)serialisable::owner_to_id_to_pointer[owner_id][id];
+
+        bool was_nullptr = ptr == nullptr;
+
+        if(was_nullptr)
+        {
+            ptr = new T();
+        }
+
+        serialise_helper<T> data_fetcher;
+        *ptr = data_fetcher.get(internal_counter, data);
+
+        if(was_nullptr)
+        {
+            ptr->handled_by_client = false;
+            ptr->owned = false;
+            ptr->owner = owner_id;
+        }
+
+        return ptr;
+    }
+};
+
+struct serialise
+{
+    std::vector<char> data;
+
+    int internal_counter = 0;
+
+    template<typename T>
+    void push_back(T v)
+    {
+        serialise_helper<T> helper;
+
+        helper.add(v, data);
+    }
+
+    ///if pointer, look up in pointer map
+    template<typename T>
+    T get()
+    {
+        serialise_helper<T> helper;
+
+        return helper.get(internal_counter, data);
+    }
+
+    template<typename T>
+    void handle_serialise(T& v, bool serialise)
+    {
+        if(serialise)
+        {
+            push_back(v);
+        }
+        else
+        {
+            v = get<T>();
+        }
+    }
+
+    bool finished_deserialising()
+    {
+        return internal_counter >= (int)data.size();
+    }
+};
+
+
+#endif // SERIALISE_HPP_INCLUDED
