39f882dba4a8c724f7746317d1003affd0999c74
James <Icedshot@gmail.com>
2017-09-12 23:31:40 +0100

    migrate scrapping ship into context menu

--- ship.cpp
+++ ship.cpp
@@ -1876,7 +1876,11 @@ void ship::context_handle_menu(orbital* o, empire* player_empire)
 
     bool get_research = original_owning_race != player_empire;
 
-    if(owned && not_busy_and_in_friendly_territory)
+    bool can_claim_hostile = (player_empire == o->parent_system->get_base()->parent_empire ||
+                             player_empire->is_allied(o->parent_system->get_base()->parent_empire)) &&
+                              !o->data->any_in_combat();
+
+    if((owned && not_busy_and_in_friendly_territory) || (fully_disabled() && can_claim_hostile))
     {
         research research_raw;
 
@@ -1887,7 +1891,10 @@ void ship::context_handle_menu(orbital* o, empire* player_empire)
 
         auto res = resources_received_when_scrapped();
 
-        res[resource::RESEARCH] = research_raw.units_to_currency(true);
+        if(get_research)
+        {
+            res[resource::RESEARCH] = research_raw.units_to_currency(true);
+        }
 
         resource_manager rm;
 
